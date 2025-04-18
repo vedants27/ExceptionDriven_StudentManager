191605e5bbc3d4bdd6365fdf641ac422dd913d0d
vedants27 <vedantss27@gmail.com>
2025-04-19 00:37:02 +0530

    Added a method to add student with duplicate and CGPA check.

--- StudentOperations.java
+++ StudentOperations.java
@@ -8,3 +8,17 @@ class StudentOperations {
     public StudentOperations() {
         this.students = new ArrayList<>();
     }
+    public void addStudent(Student student) throws DuplicateStudentException, InvalidCGPAException {
+        for (Student s : students) {
+            if (s.getPRN() == student.getPRN()) {
+                throw new DuplicateStudentException("Student with PRN " + student.getPRN() + " already exists.");
+            }
+        }
+
+        if (student.getCGPA() < 0.0 || student.getCGPA() > 10.0) {
+            throw new InvalidCGPAException("CGPA must be between 0 and 10.");
+        }
+
+        students.add(student);
+        System.out.println("Student added.");
+    }
