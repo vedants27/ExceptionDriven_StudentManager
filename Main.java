cd0a5c55d311ba2faa5672229284c17b65f6ea36
vedants27 <vedantss27@gmail.com>
2025-04-18 23:58:37 +0530

    Created main class and scanner setup.

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
 // Main.java
+// Name: Vedant Shitole
+// PRN: 23070126143
+// Batch: AIML B3
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations();
